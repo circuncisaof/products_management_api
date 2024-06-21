82706578ec3431e6ca194c6d4390fa607e53f71f
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-21 14:14:54 -0300

    feat: add department entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartmentModule } from './departments/department.module';
+import { DepartmenttEntity } from './departments/entities/department.entity';
 import { ProductEntity } from './products/entities/product.entity';
 import { ProductsModule } from './products/product.module';
 
 @Module({
   imports: [
     ProductsModule,
+    DepartmentModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -13,7 +16,7 @@ import { ProductsModule } from './products/product.module';
       username: 'postgres',
       password: 'mari0001',
       database: 'postgres',
-      entities: [ProductEntity],
+      entities: [ProductEntity, DepartmenttEntity],
       synchronize: true,
     }),
   ],
