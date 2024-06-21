309a78aa269bd06b39deb5e5ac8af6915a2cf33c
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-21 09:47:54 -0300

    deleted i think

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './products/entities/product.entity';
+import { ProductsModule } from './products/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'mari0001',
+      database: 'postgres',
+      entities: [ProductEntity],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
