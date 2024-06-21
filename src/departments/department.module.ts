b78268f7d34923119a0dda442fbdae80069b7fa3
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-21 14:15:08 -0300

    feat: department module

--- src/departments/department.module.ts
+++ src/departments/department.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DepartmentController } from './department.controller';
+import { DepartmentService } from './department.service';
+import { DepartmenttEntity } from './entities/department.entity';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([DepartmenttEntity])],
+  controllers: [DepartmentController],
+  providers: [DepartmentService],
 })
 export class DepartmentModule {}
