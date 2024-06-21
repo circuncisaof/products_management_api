a4eb31d8d51f95796bf42527e732530be9c61423
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-21 11:00:43 -0300

    feat: module typeorm

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/product.entity';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
   controllers: [ProductController],
   providers: [ProductService],
 })
