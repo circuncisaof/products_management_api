b79a76c50e31dc204cc36b07acbfb5d1625559f0
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-24 14:44:23 -0300

    cart

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CartService } from './cart.service';
 import { InsertCart } from './dtos/cart.dto';
 
@@ -7,11 +15,13 @@ export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Post()
+  @HttpCode(HttpStatus.CREATED)
   async create_card(@Body() data: InsertCart) {
     return this.cartService.create_card(data);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   async delete_cart(@Param('id') id: string) {
     return await this.cartService.delete_cart(id);
   }
