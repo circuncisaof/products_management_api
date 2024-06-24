7e47a56f15a63e3dce01edd8837b57a65222bbb3
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-24 10:34:38 -0300

    feat: return id in file returndepartment

--- src/departments/dtos/return_department/return_department.dto.ts
+++ src/departments/dtos/return_department/return_department.dto.ts
@@ -2,7 +2,9 @@ import { DepartmenttEntity } from 'src/departments/entities/department.entity';
 
 export class ReturnDepartment {
   name: string;
+  id: string;
   constructor(department_entity: DepartmenttEntity) {
     this.name = department_entity.name;
+    this.id = department_entity.id;
   }
 }
